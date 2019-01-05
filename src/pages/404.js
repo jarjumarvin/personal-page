b4d17fc43bbf9b866ddb1b849ae34bdb620c8fd3
Marvin Jarju <marvin.jarju@gmail.com>
2019-01-05 20:30:03 +0100

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/404.scss';
+
+const NotFoundPage = () => (
+  <div className="page_not_found_container">
+    <div className="page_not_found">
+      <h1>oops!</h1>
+      <p>This page does not seem to exist.</p>
+    </div>
+  </div>
+);
+
+export default NotFoundPage;
