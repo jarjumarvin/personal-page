5f27b9fca79490991c761faabca7da831a1b1e7f
Marvin Jarju <marvin.jarju@gmail.com>
2019-01-06 13:34:16 +0100

    clean config, add favico

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Helmet from 'react-helmet';
+import favico from '../images/favico.png';
 import '../scss/index.scss';
 
 import Title from '../components/Title';
@@ -7,9 +9,15 @@ import Terminal from '../components/Terminal';
 
 import prompts from '../data/about';
 
+
 const Home = () => (
   <div className="container-lg">
-    <Helmet title="Marvin Jarju | personal page" />
+    <Helmet
+      title="Marvin Jarju | personal page"
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favico}` },
+      ]}
+    />
     <div className="home-container">
       <div className="home-landing">
         <Title />
